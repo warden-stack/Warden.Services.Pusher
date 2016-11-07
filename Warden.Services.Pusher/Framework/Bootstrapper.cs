934ebd906ab0b7c715bf4bdf51c50015260bff77
Piotr Gankiewicz <spetz90@gmail.com>
2016-11-07 12:30:38 +0100

    Updated raw rabbit settings

--- Warden.Services.Pusher/Framework/Bootstrapper.cs
+++ Warden.Services.Pusher/Framework/Bootstrapper.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNet.SignalR;
 using Microsoft.Extensions.Configuration;
 using RawRabbit;
 using RawRabbit.vNext;
+using RawRabbit.Configuration;
 using Warden.Common.Events;
 using Warden.Common.Events.Wardens;
 using Warden.Common.Nancy;
+using Warden.Common.Extensions;
 using Warden.Services.Pusher.Handlers;
 using Warden.Services.Pusher.Hubs;
 
@@ -26,7 +28,10 @@ namespace Warden.Services.Pusher.Framework
             base.ConfigureApplicationContainer(container);
             container.Update(builder =>
             {
-                builder.RegisterInstance(BusClientFactory.CreateDefault()).As<IBusClient>();
+                var rawRabbitConfiguration = _configuration.GetSettings<RawRabbitConfiguration>();
+                builder.RegisterInstance(rawRabbitConfiguration).SingleInstance();
+                builder.RegisterInstance(BusClientFactory.CreateDefault(rawRabbitConfiguration))
+                    .As<IBusClient>();
                 builder.RegisterInstance(GlobalHost.ConnectionManager.GetHubContext<WardenHub>()).As<IHubContext>();
                 builder.RegisterType<SignalRService>().As<ISignalRService>();
                 builder.RegisterType<WardenCheckResultProcessedHandler>()
