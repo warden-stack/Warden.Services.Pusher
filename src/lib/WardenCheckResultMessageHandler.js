e0696f5b9da272515b52b66da50ad59af6ec6fb9
Piotr Gankiewicz <spetz90@gmail.com>
2017-05-06 23:51:32 +0200

    Fixed emitted event:

--- src/lib/WardenCheckResultMessageHandler.js
+++ src/lib/WardenCheckResultMessageHandler.js
@@ -5,6 +5,7 @@ module.exports = function(io) {
 
   this.publishWardenCheckResultProcessed = (event) => {
     console.log('Publishing warden_check_result_processed message');
-    this.io.sockets.emit('warden_check_result_processed', event);
+    console.log(event.checkResult)
+    this.io.sockets.emit('warden_check_result_processed', event.checkResult);
   };
 }
